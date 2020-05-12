6d3e33f40a4a3ff3e8bbd7589f08de91a140fa92
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-05-12 13:46:58 +0200

    added drawPoint method

--- src/Zeichnenprogramm/Draw.java
+++ src/Zeichnenprogramm/Draw.java
@@ -6,6 +6,9 @@
 package Zeichnenprogramm;
 
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 /**
  *
@@ -13,20 +16,39 @@ import javafx.scene.input.MouseEvent;
  */
 public class Draw {
     
+    private Pane pane;
+
+    public Draw(Pane pane) {    
+        this.pane = pane;
+    }
+    
+    
+    
+    
      public void onMouseReleased(MouseEvent event) {
         System.out.println("released");
     }
 
     public void onMouseDragged(MouseEvent event) {
         System.out.println("dragged");
+        drawPoint(event.getX(), event.getY());
     }
 
     public void onMousePressed(MouseEvent event) {
         System.out.println("pressed");
+        drawPoint(event.getX(), event.getY());
     }
 
     public void onMouseMoved(MouseEvent event) {
-        System.out.println("x: " + event.getX() + "\ty: " + event.getY());
+        System.out.println("moved");
+    }
+ 
+    
+    private void drawPoint(double x, double y){
+        Circle circle = new Circle(x,y,5, Paint.valueOf("blue"));
+        pane.getChildren().add(circle);
+        
+        
     }
     
 }
